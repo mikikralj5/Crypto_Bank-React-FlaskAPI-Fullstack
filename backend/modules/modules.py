0ec2572d4b16d3ad2261aa4b9de5586e321c2237
Rade99 <noblice12345@gmail.com>
2022-10-27 00:36:48 +0200

    sending otp

--- backend/modules/modules.py
+++ backend/modules/modules.py
@@ -2,18 +2,43 @@ import string
 import random
 from config import db, mail
 from flask_mail import Message
+import requests
 
 
 def send_mail(user):
     letters = string.ascii_letters
     user.otp = ''.join(random.choice(letters) for i in range(5))
     db.session.commit()
-    msg = Message(subject="Verification Code",
-                  sender="mailzaaplikaciju21@gmail.com", recipients=[user.email])
-    msg.body = "Email Verification code = " + user.otp
-    try:
-        mail.send(msg)
-    except:
+    import requests
+
+    url = "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send"
+
+    payload = {
+        "personalizations": [
+            {
+                "to": [{"email": user.email}],
+                "subject": "Verification Code"
+            }
+        ],
+        "from": {"email": "crypto_bros_inc@gmail.com"},
+        "content": [
+            {
+                "type": "text/plain",
+                "value": "Email Verification code = " + user.otp
+            }
+        ]
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "932eab884cmshd9f6feb1e4c4f13p12e23ajsn7e8373a8eb9c",
+        "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    if response.status_code == 202:
+        return True
+    else:
         return False
 
 def update_crypto_currency(name, amount, crypto_currencies):
